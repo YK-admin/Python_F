24be5736f75972ec47d786b41d501d4e7f54bcda
YK-admin <kikuchi.yuta.qe@mynavi.jp>
2023-05-19 05:21:53 +0000

    fix population.py

--- kikuchi/05_19/P52_population.py
+++ kikuchi/05_19/P52_population.py
@@ -13,3 +13,6 @@ if num <= 0:
 countries = ("China", "India", "U.S.A", "Indonesia", "Pakistan", "Brazil", "Nigeria", "Bangladesh", "Russia", "Mexico")
 
 print(countries[num-1], end="")
+
+# import sys
+# print(("China", "India", "U.S.A", "Indonesia", "Pakistan", "Brazil", "Nigeria", "Bangladesh", "Russia", "Mexico")[int(sys.argv[1])-1], end="")
