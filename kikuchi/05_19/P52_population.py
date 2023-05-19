20160b6115162a9c4b4309b3e9a58e6e461630a7
YK-admin <kikuchi.yuta.qe@mynavi.jp>
2023-05-19 05:16:48 +0000

    add population.py

---
+++ kikuchi/05_19/P52_population.py
@@ -0,0 +1,15 @@
+import sys
+args = sys.argv
+
+# 引数代入
+num = int(args[1])
+
+# 引数が0ならエラー
+if num <= 0:
+    print("ValueError")
+    exit()
+
+# 国リスト
+countries = ("China", "India", "U.S.A", "Indonesia", "Pakistan", "Brazil", "Nigeria", "Bangladesh", "Russia", "Mexico")
+
+print(countries[num-1], end="")
