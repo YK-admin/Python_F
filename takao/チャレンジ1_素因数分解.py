e7a9d9f1f11d5421076f16b67dd40a2a560f01aa
TakaoYuna <takao.yuna.ai@mynavi.jp>
2023-05-19 06:55:01 +0000

    max削除

--- takao/チャレンジ1_素因数分解.py
+++ takao/チャレンジ1_素因数分解.py
@@ -1,18 +1,13 @@
 # 引数で渡した数字の素因数分解を出力
 import sys
 args = sys.argv
-
 # 引数を変数に代入
 num = int(args[1])
-
 # 素因数分解のリスト作成
 prime_list = []
-
 # 割り切れたら素因数分解のリストに追加
-
 i = 2 
-max = num
-while i <= max:
+while i <= num:
     if num % i == 0:
         prime_list.append(i)
         num = num / i 
