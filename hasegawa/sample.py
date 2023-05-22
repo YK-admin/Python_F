3ef7c5a582ac651919a473b0650ca21a43b3e629
TakumiHasegawa321 <hasegawa.takumi.rf@mynavi.jp>
2023-05-22 08:26:39 +0000

    add file:

---
+++ hasegawa/sample.py
@@ -0,0 +1,18 @@
+class Intro:
+    def __init__(self):
+        self.name = None
+        self.age = None
+
+
+sample = Intro()
+
+sample.name = "takumi"
+sample.age = 30
+
+print(sample.name)
+print(sample.age)
+
+sample2 = Intro()
+print(sample2.name)
+print(sample2.age)
+
