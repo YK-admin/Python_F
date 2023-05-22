7273a9a7d66c65a6149586a19346d468b013fb8d
s12300226 <nakagawa.yuki.jy@mynavi.jp>
2023-05-22 02:33:25 +0000

    add func-mul-simple.py

--- penguinsan/0522/func_oddeven.py
+++ penguinsan/0522/func_oddeven.py
@@ -2,15 +2,14 @@ import sys
 
 # 関数を定義
 def calcvalue(num):
-    
     # あまりを算出
     mod = num % 2
 
     # あまりの値から奇数偶数判定
     if mod != 0:
-        print(str(num) + "は奇数")
+        print(str(num) + "は奇数",end="")
     else:
-        print(str(num) + "は偶数")
+        print(str(num) + "は偶数",end="")
 
 # 引数の取得
 args = sys.argv[1:]  # プログラム名を除いた引数を取得
