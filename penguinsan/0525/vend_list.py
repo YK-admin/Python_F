cdecbec1c64e5bb742461d39acc0f74b4be78fe4
s12300226 <nakagawa.yuki.jy@mynavi.jp>
2023-05-25 06:31:33 +0000

    add 2 files

--- penguinsan/0525/vend_list.py
+++ penguinsan/0525/vend_list.py
@@ -5,5 +5,9 @@ def product_list(vend_list):
         ("コーヒー", 100),
         ("ソーダ", 160),
         ("コーンポタージュ", 130)
-]
+    ]
+
+    print(vend_list)
+
+product_list(None)  # 引数には適当な値を渡すか、Noneを渡してください
 
